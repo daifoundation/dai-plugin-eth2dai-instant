ad8a8046a16505b56faefb773d0fde2349406a8f
Ethan <ebennett19@gmail.com>
2019-04-22 22:50:40 -0600

    Add build script

---
+++ scripts/build.sh
@@ -0,0 +1,17 @@
+# #!/usr/bin/env bash
+
+set -e
+
+if [ "$1" = "dirty" ]; then
+  echo "Dirty mode: not removing previous build files."
+else
+  rm -rf dist
+fi
+
+babel src --out-dir ./dist/src
+
+copyfiles \
+  README.md \
+  LICENSE \
+  package.json \
+  dist
